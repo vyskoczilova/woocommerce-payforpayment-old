ea42ba9a77d6eaba573c941779e3acc46b1edda2
mcguffin <joern@flyingletters.com>
2013-11-06 11:23:41 +0100

    l10n, increment version

--- woocommerce-payforpayment.php
+++ woocommerce-payforpayment.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Pay for Payment
 Plugin URI: https://github.com/mcguffin/woocommerce-payforpayment
 Description: Add extra fees depending on your payment methods.
-Version: 0.0.2
+Version: 0.0.3
 Author: Jörn Lund
 Author URI: https://github.com/mcguffin
 
