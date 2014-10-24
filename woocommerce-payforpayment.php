c49c356838202b9298a5f6dc53718f994fb86bdb
Jörn Lund <joern@flyingletters.com>
2014-10-24 09:58:20 +0200

    change version identifier

--- woocommerce-payforpayment.php
+++ woocommerce-payforpayment.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Pay for Payment
 Plugin URI: http://wordpress.org/plugins/woocommerce-pay-for-payment
 Description: Setup individual charges for each payment method in woocommerce.
-Version: 1.3.3
+Version: 1.3.3-multi-step-fix
 Author: Jörn Lund
 Author URI: https://github.com/mcguffin
 License: GPL
